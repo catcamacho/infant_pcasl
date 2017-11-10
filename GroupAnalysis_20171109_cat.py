3610c53507449a74211cac0dc09052626018b8c3
catcamacho <m.catalina.camacho@gmail.com>
2017-11-10 08:51:18 -0500

    add node to mask data before randomise

--- GroupAnalysis_20171109_cat.py
+++ GroupAnalysis_20171109_cat.py
@@ -8,6 +8,7 @@ from nipype.interfaces.io import SelectFiles, DataSink, DataGrabber
 from nipype.pipeline.engine import Workflow, Node, MapNode
 from nipype.interfaces.fsl.utils import Merge
 from nipype.interfaces.fsl.model import Randomise
+from nipype.interfaces.fsl.maths import ApplyMask
 
 # FSL set up- change default file output type
 from nipype.interfaces.fsl import FSLCommand
@@ -19,6 +20,7 @@ project_home = '/Users/catcamacho/Dropbox/Projects/infant_ASL/proc'
 output_dir = project_home + '/Proc'
 wkflow_dir = project_home + '/Workflows'
 template = project_home + '/Templates/T2wtemplate_2mm.nii'
+mask = project_home + '/Templates/standard_mask.nii'
 
 # Files for group level analysis
 group_mat = project_home + '/Misc/group.mat'
@@ -45,6 +47,8 @@ grabcbfdata = Node(DataGrabber(template=output_dir + '/warped_cbf_vol/*/salsop_c
 
 merge = Node(Merge(dimension = 't'), name = 'merge')
 
+apply_mask = Node(ApplyMask(mask_file=mask), name='apply_mask')
+
 randomise = Node(Randomise(tfce = True,
                            num_perm = 500,
                            tcon = t_contrasts,
@@ -58,7 +62,8 @@ randomise = Node(Randomise(tfce = True,
 grouplevel = Workflow(name='grouplevel')
 
 grouplevel.connect([(grabcbfdata, merge,[('cbf_list', 'in_files')]),
-                    (merge, randomise, [('merged_file', 'in_file')]),
+                    (merge, apply_mask, [('merged_file','in_file')]),
+                    (apply_mask, randomise, [('out_file', 'in_file')]),
                     (randomise, datasink, [('t_corrected_p_files', 't_corrected_p_files')]),
                     (randomise, datasink, [('t_p_files', 't_p_files')]),
                     (randomise, datasink, [('tstat_files', 'tstat_files')])
